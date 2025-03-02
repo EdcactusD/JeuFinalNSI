e43713e250ddf1fc2efee5e25bad8c49bda348cc
Lounis04 <Lounisdany@gmail.com>
2025-03-02 19:59:38 +0100

    Add files via upload

---
+++ test sépération fichiers/Mars.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+from Map import Map
+
+class Mars(Map):
+    def __init__(self, jeu):
+        super().__init__(jeu)
+        self.bg_image = pygame.image.load(os.path.join("assets","fonds", "Vitesse.jpg"))
+        self.bg_image = pygame.transform.scale(self.bg_image, (self.jeu.bg_width, self.jeu.bg_height))
+
+    def handle_events(self, event):
+        super().handle_events(event)
---
+++ test sépération fichiers/Reglages.py
@@ -0,0 +1,6 @@
+import os 
+import pygame
+import Etats
+
+class Reglages():
+  pass

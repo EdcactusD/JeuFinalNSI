45ac7d806c041e48bc0d30c7ee653a36d0930db3
Mip-p <drouetmila@gmail.com>
2025-04-13 00:43:50 +0200

    tir arc [mais jpense que y'a un plb dans la gestion des niveaux]
    en gros c'est fonctionnel (juste j'ai pas mis aide et reglage parcque j'attend qu'on fasse un truc general (pour pas copier-coller)
    >>> sinon y a des bugs de temps en temps dans le passage des niveaux, genre le niveau devrait etre passé mais il se passe avec un temps de retard (apres un autre clic : je pense que le clic est déclancheur du passage de niveau) mais c'est pas systémique

--- essai3.py
+++ essai3.py
@@ -3,6 +3,7 @@ import os
 import time
 import random
 import math
+from math import sqrt
 
  
 """
@@ -98,7 +99,7 @@ class Etats(): #SUPERCLASSE : la classe qui gère tous les etats du jeu
                             "Pendu" : [0, "",],
                             "Pendule" : [0, "Cliquez sur le\nbouton stop au\nbon moment\npour arreter\nles aiguilles",],
                             "Portes" : [0, "",],
-                            "Tir_arc" :[0, "",],
+                            "Tir_arc" :[0, "Cliquez sur l'écran pour tirer une flèche\nle niveau est passé si elle atteint la cible\nà la fin de la trajectoire",],
                             "Vitesse" : [0, "Ecrivez les mots\nles plus rapidement\n possibles en \nrespectant le délai\n des 5 secondes",],
                             "Bon_minerai" :[0, "Associez le bon\n nom au bon minerai",],
                             "Trad" : [0, "",],
@@ -596,13 +597,125 @@ class Portes(Etats):
         
 
 class Tir_arc(Etats):
+    #ATTENTION : comme les regles et aide ne sont pas dans la super classe ; les conditions ne changent pas le curseur dessus
     def __init__(self, jeu):
         super().__init__(jeu)
         self.bg_image = pygame.image.load(os.path.join("assets","fonds", "Tir_arc.jpg"))
         self.bg_image = pygame.transform.scale(self.bg_image, (self.jeu.bg_width, self.jeu.bg_height))
+        self.curseur_img= pygame.image.load(os.path.join("assets", "viseur.png"))
+        self.curseur_img = pygame.transform.scale(self.curseur_img, (self.jeu.bg_width/11, self.jeu.bg_width/11 )) #on fait que par rapport à width car on veut un carré
+        self.fleche_img=pygame.image.load(os.path.join("assets", "fleche.png"))
+        self.fleche_img_wh =self.jeu.bg_width/15
+        self.fleche_img = pygame.transform.scale(self.fleche_img, (self.fleche_img_wh, self.fleche_img_wh))
+        
+        self.cible_img = pygame.image.load(os.path.join("assets", "cible.png"))
+        
+        self.niveau = str(self.niveaux_jeux["Tir_arc"][0])
+        self.niveau_increment = False #permet d'incrementer 1 fois par bon tir le niveau
+        
+        self.dico_niveaux = { "0" : { "cible_taille" : self.jeu.bg_width//4},
+                              "1" : { "cible_taille" : self.jeu.bg_width//6},
+                              "2" : { "cible_taille" : self.jeu.bg_width//6,  "vitesse_cible" : 0.75},
+                              "3" : { "cible_taille" : self.jeu.bg_width//6,  "vitesse_cible" : 1.25},
+                              "4" : { "cible_taille" : self.jeu.bg_width//6,  "vitesse_cible" : 1.8}
+            }
+        
+        #pour les niveaux 2 et 4, où la cible va bouger
+        self.cible_pos_bouge = self.jeu.bg_width//2-self.dico_niveaux["2"]["cible_taille"]//2 #on prend la taille de niv.2, qui est la même pour niv.4
+        
+        self.direction= 1 #1 pour vers la gauche, -1 pour vers la droite
+        self.distance_cible_max=200
 
+        self.en_tir= True
+        self.en_vol=False
+         
+        self.tir_x = 0 #position finale de la fleche une fois tout le mouvement effectué, mise à 0 pour eviter les bugs (car cible ne sera jamais à 0;0 donc pas de problemes et ce sont des valeurs de coordonées possibles donc pas de modifiactions dans la logique du code)
+        self.tir_y = 0
+        self.vitesse = 15 #represente la vitesse de la fleche a parcourir la trajectoire
+        
+        self.hauteur = 300
+        self.longueur = 210
+        
+
+    
     def handle_events(self, event):
-        super().handle_events(event)  
+        self.cible_taille = self.dico_niveaux[self.niveau]["cible_taille"]
+        self.cible_pos = (self.jeu.bg_width//2-self.cible_taille//2,self.jeu.bg_height//2-self.cible_taille//2)
+        self.cible_img = pygame.transform.scale(self.cible_img, (self.cible_taille, self.cible_taille )) #l'image de base est un carré
+        self.rond_cible = { "centre" : (self.cible_pos[0] + self.cible_taille//2 , self.cible_pos[1] + self.cible_taille//2), #car sur l'image le centre de la cible est le centre de l'image
+                            "rayon" : self.cible_taille//6 , 
+            }
+        
+        super().handle_events(event)
+        #le get_rect crée un rect pour le menu, le topleft le positionne au bon endroit (à partir du haut gauche comme dans le reste du programme) sinon il va en (0,0)
+        if (event.type == pygame.MOUSEMOTION and self.menu.get_rect(topleft=(self.menu_x, self.menu_y)).collidepoint(event.pos) and self.show_menu):
+            #or (event.type == pygame.MOUSEMOTION and self.regles.collidepoint(event.pos)) or (event.type == pygame.MOUSEMOTION and self.aide.collidepoint(event.pos))
+            self.en_tir= False
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+            pygame.mouse.set_visible(True)
+        else :
+            self.en_tir = True
+            pygame.mouse.set_visible(False) #car on va afficher une image pour remplacer le curseur (viseur) 
+            
+        self.x_souris, self.y_souris = pygame.mouse.get_pos() #on le met dans handle_events pour qu'il change bien à chaque fois
+        
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.en_tir and not self.en_vol: #on verfie si on peut lancer une fleche
+           self.deb_temps_en_vol = time.time()
+           self.en_vol=True
+           self.niveau_increment=False 
+            #enregistre le moment du début du mouvement
+           self.tir_x_base,self.tir_y_base=self.x_souris, self.y_souris
+           #on déduis les coordonées de alpha et beta
+           self.alpha = self.longueur/2 #alpha est la longeur/2 car alpha est la coordonée en x du sommet, axe de symétrie de la parabole // on definit les caractéristiques de notre trajectoire
+           self.beta= self.hauteur
+           self.a = -self.beta/(self.alpha**2) #on trouve a tel que les points sont sur la parabole
+
+        
+        #self.tir_x est calculé dand le draw pour une meilleure fluidité     
+        self.distance_cible_fleche= sqrt((self.rond_cible["centre"][0]-self.tir_x)**2 + (self.rond_cible["centre"][1]-self.tir_y)**2) #théroème de Pythagore
+        if self.distance_cible_fleche<= self.rond_cible["rayon"] and self.tir_x_base-self.tir_x>self.longueur/2:  #on vérifie qu'on est assez avancé dans la trajectoire (éviter qu'on puisse juste viser directement sur al cible)
+              if not self.niveau_increment and self.niveau!="4":
+                  self.niveau=str(int(self.niveau)+1)
+                  self.niveau_increment=True
+                  print("niveau en cours: ", self.niveau)
+              if not self.niveau_increment and self.niveau=="4": #le mini-jeu est fini
+                 self.jeu.changer_etat(Map(self.jeu))
+    
+    def draw(self, screen):
+        self.cible_taille = self.dico_niveaux[self.niveau]["cible_taille"]
+        self.cible_pos = (self.jeu.bg_width//2-self.cible_taille//2,self.jeu.bg_height//2-self.cible_taille//2)
+        self.cible_img = pygame.transform.scale(self.cible_img, (self.cible_taille, self.cible_taille )) #l'image de base est un carré
+        self.rond_cible = { "centre" : (self.cible_pos[0] + self.cible_taille//2 , self.cible_pos[1] + self.cible_taille//2), #car sur l'image le centre de la cible est le centre de l'image
+                            "rayon" : self.cible_taille//6 , 
+            }
+        
+        super().draw(screen)
+        if self.niveau=="2" or self.niveau=="3" or self.niveau=="4":  
+            if abs(self.cible_pos[0]-self.cible_pos_bouge) >= self.distance_cible_max: #on recupere la valeur absolue (donc la longueur qui spéare les deux éléments)
+                self.direction*=-1
+            self.cible_pos_bouge+=self.dico_niveaux[self.niveau]["vitesse_cible"]*self.direction
+            screen.blit(self.cible_img, (self.cible_pos_bouge, self.cible_pos[1]))
+        else:
+          screen.blit(self.cible_img, self.cible_pos)
+        if self.en_tir:
+            souris_pos = pygame.mouse.get_pos()
+            screen.blit(self.curseur_img, (souris_pos[0] - self.curseur_img.get_width() // 2, #on blit pour que le centre de l'image soit blit où il y a le curseur (si on ne divise pas par 2 ce sera le coin gauche au niveau du curseur)
+                                           souris_pos[1] - self.curseur_img.get_height() // 2)) 
+        if self.en_vol:
+            self.temps_passe_en_vol = time.time() - self.deb_temps_en_vol
+            
+            self.tir_x = self.tir_x_base - self.vitesse* self.temps_passe_en_vol*10
+            self.tir_y =self.tir_y_base - self.a*(((self.tir_x_base-self.tir_x)-self.alpha)**2)-self.beta #on a calculé l'image dans le repere d'origine (tir_x_base;tir_y_base) donc on ajoute tir_y_base pour que la position soit bonne + on change le signe du trinôme pour que les branches soient vers le haut
+            
+
+
+            # Vérifier si la distance entre le point de départ et le point actuel dépasse un certain seuil
+            if self.tir_x_base - self.tir_x >= self.longueur:
+                self.en_vol = False  # Arrêter le mouvement lorsque la distance est atteinte
+            
+            screen.blit(self.fleche_img, (self.tir_x-self.fleche_img_wh//2, self.tir_y-self.fleche_img_wh//2)) #on blit l'image à partir de son centre (si on enleve rien c'est au coin supérieur gauche)
+            
+
         
 
 class Vitesse(Etats):
